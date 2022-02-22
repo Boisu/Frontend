2e7d6f842bce87c46b1ac180905f5baa79cb25af
M. Faiz Triputra <m.faiztpaduhai@gmail.com>
2022-02-22 22:44:52 +0700

    checkpoint axios

--- src/view/ListComponent.js
+++ src/view/ListComponent.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 import { ListGroup } from "react-bootstrap";
+import axios from "axios";
 
 export default class ListComponent extends Component {
   alertClicked() {
     alert("You clicked the third ListGroupItem");
+  }
+
+  componentDidMount() {
+    const config = {};
+    axios
+      .get("https://boisu-api.mfaiztriputra.id/api/words", {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render(alertClicked) {
--- src/view/index.js
+++ src/view/index.js
@@ -3,20 +3,37 @@ import NavbarComponent from "./NavbarComponent";
 import ListComponent from "./ListComponent";
 import FindComponent from "./FindComponent";
 import ButtonComponent from "./ButtonComponent";
+import AddIcon from '@mui/icons-material/Add';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
 
 export default class Homepage extends Component {
   render() {
     return (
-      <div className="container">
-        <NavbarComponent />
-        <div className="container mt-4">
-          <FindComponent />
-          <ListComponent />
+      <>
+        <div className="container position-relative px-2">
+          <NavbarComponent />
+          <div className="">
+            <FindComponent />
+            <ListComponent />
+          </div>
         </div>
-        <div className="text-center">
-          <ButtonComponent />
+        <div className="d-flex justify-content-between">
+          <div className="position-absolute bottom-0 right-0 text-right mb-5">
+            <ButtonComponent icon={<CheckIcon />} color="bg-success text-white" />
+          </div>
         </div>
-      </div>
+        <div className="d-flex justify-content-center">
+          <div className="position-absolute bottom-0 right-0 text-right mb-5">
+            <ButtonComponent icon={<AddIcon />} color="bg-warning text-white" />
+          </div>
+        </div>
+        <div className="d-flex justify-content-end">
+          <div className="position-absolute bottom-0 right-0 text-right mb-5">
+            <ButtonComponent icon={<ClearIcon />} color="bg-danger text-white" />
+          </div>
+        </div>
+      </>
     );
   }
 }
